4c5b9fd02a1be7239b808c73269d0fda02fe62a8
unnatikhatri <59996056+unnatikhatri@users.noreply.github.com>
2020-01-26 22:28:56 +0530

    Add files via upload

---
+++ Promact/Employee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Promact
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+        private string deptName;
+
+        public Employee(int id_ex, string name_ex, string deptName_ex)
+        {
+            Id = id_ex;
+            Name = name_ex;
+            DeptName = deptName_ex;
+        }
+        public void GetId()
+        {
+            Console.WriteLine("GetId() method called.");
+            Console.WriteLine(Id);
+        }
+
+        public void GetName()
+        {
+            Console.WriteLine("GetName() mathod called.");
+            Console.WriteLine(Name);
+        }
+
+        public void GetDeptName()
+        {
+            Console.WriteLine("GetDeptName() mathod called.");
+            Console.WriteLine(DeptName);
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string DeptName
+        {
+            get
+            {
+                return deptName;
+            }
+            set
+            {
+                deptName = value;
+            }
+        }
+ 
+            
+
+    }
+}
---
+++ Promact/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Promact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string name;
+            string deptName;
+
+            Console.WriteLine("Enter Id : ");
+            id= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Name : ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Department Name : ");
+            deptName = Console.ReadLine();
+
+            Employee employee1 = new Employee(id, name, deptName);
+            employee1.GetId();
+            employee1.GetName();
+            employee1.GetDeptName();
+
+        }
+    }
+}
